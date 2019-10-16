c20fe30107b41689eb9311be6a10a095262d9a92
Jonathan Forney <jonathan.forney@gmail.com>
2019-10-15 21:29:20 -0700

    this is probably close to good but might need tweaks

--- src/services/nasa-api.js
+++ src/services/nasa-api.js
@@ -1,22 +1,25 @@
 //Our NASA API calls are here - basic structure take from my Rick And Morty thing
+import default_key from '../../key';
 
 const url = 'https://api.nasa.gov/planetary/apod';
-const
+const key = default_key;
+
 
 export const getImage = (key, date) => {
+
   console.log('here in api fetch');
-  return fetch(`${url}`)
+  return fetch(`${url}${key}${date}`)
     .then(res => ([res.ok, res.json()]))
     .then(([ok, json]) => {
       if(!ok) throw 'Unable to fetch Astronomy Photo';
 
       return json;
     })
-    .then(({ results }) => results.map(photo => ({
-      date: photo.date,
-      explanation: photo.explanation,
-      hdurl: photo.hdurl,
-      title: photo.title,
-      url: photo.url,
+    .then(({ results }) => results.map(astroPhoto => ({
+      date: astroPhoto.date,
+      explanation: astroPhoto.explanation,
+      hdurl: astroPhoto.hdurl,
+      title: astroPhoto.title,
+      url: astroPhoto.url,
     })));
 };
