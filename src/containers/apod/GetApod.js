c1f202b532a2f1e21f48f315aece805fbe3292b3
Jonathan Forney <jonathan.forney@gmail.com>
2019-10-21 16:49:07 -0700

    something is wrong with way i instanciate ApodComp but not sure what

--- src/containers/apod/GetApod.js
+++ src/containers/apod/GetApod.js
@@ -1,7 +1,7 @@
 import ApodComp from '../../components/apod/Apod';
 // import { getImage } from '../../services/nasa-api';
 
-const testObj = {
+const astroPhoto = {
   date: '2017-04-16',
   explanation: 'Does Enceladus have underground oceans that could support life?',
   hdurl: 'https://apod.nasa.gov/apod/image/1704/EnceladusShadow_Cassini_960.jpg',
@@ -10,4 +10,4 @@ const testObj = {
 };
 
 // export default ApodComp(getImage);
-export default ApodComp({ testObj });
+export default ApodComp({ astroPhoto });
