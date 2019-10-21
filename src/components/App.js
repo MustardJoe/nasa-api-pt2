efa74680ed818140c63f34c73945dc99a0c35e55
Jonathan Forney <jonathan.forney@gmail.com>
2019-10-21 16:48:07 -0700

    added dummy data to app for testing comp and style purposes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import GetApod from '../containers/apod/GetApod';
+// import GetApod from '../containers/apod/GetApod';
+import ApodComp from '../components/apod/Apod';
+
+const astroPhoto = {
+  date: '2017-04-16',
+  explanation: 'Does Enceladus have underground oceans that could support life?',
+  hdurl: 'https://apod.nasa.gov/apod/image/1704/EnceladusShadow_Cassini_960.jpg',
+  title: 'Life-Enabling Plumes above Enceladus',
+  url: 'https://apod.nasa.gov/apod/image/1704/EnceladusShadow_Cassini_960.jpg',
+};
 
 export default function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      <GetApod />
+      <ApodComp astroPhoto={astroPhoto} />
     </div>
   );
 }
