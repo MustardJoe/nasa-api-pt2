ad72fd94f152e3bd7c9c7b6ea79b5c655ebbd07f
Jonathan Forney <jonathan.forney@gmail.com>
2019-10-21 23:46:35 -0700

    wrote container using simple rickandmorty as ref

--- src/containers/apod/GetApod.js
+++ src/containers/apod/GetApod.js
@@ -1,13 +1,36 @@
+import React, { Component } from 'react';
 import ApodComp from '../../components/apod/Apod';
-// import { getImage } from '../../services/nasa-api';
+import { getImage } from '../../services/nasaApi';
 
-const astroPhoto = {
-  date: '2017-04-16',
-  explanation: 'Does Enceladus have underground oceans that could support life?',
-  hdurl: 'https://apod.nasa.gov/apod/image/1704/EnceladusShadow_Cassini_960.jpg',
-  title: 'Life-Enabling Plumes above Enceladus',
-  url: 'https://apod.nasa.gov/apod/image/1704/EnceladusShadow_Cassini_960.jpg',
-};
+// const astroPhoto = {
+//   date: '2017-04-16',
+//   explanation: 'Does Enceladus have underground oceans that could support life?',
+//   hdurl: 'https://apod.nasa.gov/apod/image/1704/EnceladusShadow_Cassini_960.jpg',
+//   title: 'Life-Enabling Plumes above Enceladus',
+//   url: 'https://apod.nasa.gov/apod/image/1704/EnceladusShadow_Cassini_960.jpg',
+// };
 
-// export default ApodComp(getImage);
-export default ApodComp({ astroPhoto });
+
+class GetApod extends Component {
+  state = {
+    astroPhoto: {}
+  }
+
+  fetchAstroData = () => {
+    getImage()
+      .then(({ astroPhoto }) => this.setState({ astroPhoto }));
+  }
+  
+  componentDidMount() {
+    this.fetchAstroData();
+  }
+
+  render() {
+    const { astroPhoto } = this.state;
+
+    return <ApodComp astroPhoto={astroPhoto} />;
+  }
+}
+
+// export default ApodComp({ astroPhoto });
+export default GetApod;
