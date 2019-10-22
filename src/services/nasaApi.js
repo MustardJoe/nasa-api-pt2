94ef9489c6999058044d02e5491bcbe3c9c0bd42
Jonathan Forney <jonathan.forney@gmail.com>
2019-10-21 23:45:07 -0700

    renamed api file to hold with convention

--- src/services/nasaApi.js
+++ src/services/nasaApi.js
@@ -3,13 +3,17 @@ import default_key from '../../key';
 
 const url = 'https://api.nasa.gov/planetary/apod';
 const key = default_key;
+/* eslint-disable-next-line no-console */
+console.log(key);
 
+export const getImage = (key, date) => {
 
-const getImage = (key, date) => {
-
+  /* eslint-disable-next-line no-console */
   console.log('here in api fetch');
   return fetch(`${url}${key}${date}`)
     .then(res => ([res.ok, res.json()]))
+    .then(console.log(`${url}${key}${date}`))
+    .then(console.log(`Start here in nasaApi line 16 'undefined' stuff`))
     .then(([ok, json]) => {
       if(!ok) throw 'Unable to fetch Astronomy Photo';
 
@@ -24,4 +28,4 @@ const getImage = (key, date) => {
     })));
 };
 
-export default getImage();
+
