2342bd66750fe86dcfb7008c8466234cef6cf971
Jonathan Forney <jonathan.forney@gmail.com>
2019-10-28 20:18:29 -0700

    cleaned up old code and added DateComp

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
-// import GetApod from '../containers/apod/GetApod';
-import ApodComp from '../components/apod/Apod';
 import GetApod from '../containers/apod/GetApod';
-
-const astroPhoto = {
-  date: '2017-04-16',
-  explanation: 'Does Enceladus have underground oceans that could support life?',
-  hdurl: 'https://apod.nasa.gov/apod/image/1704/EnceladusShadow_Cassini_960.jpg',
-  title: 'Life-Enabling Plumes above Enceladus',
-  url: 'https://apod.nasa.gov/apod/image/1704/EnceladusShadow_Cassini_960.jpg',
-};
+import DateComp from '../components/date/DateComp';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Jon's Nasa APOD Experience</h1>
+      <DateComp />
       <GetApod />
     </div>
   );
