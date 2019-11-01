7ae190e85a5f31c4b2d487665d4c88a25a4ca4a4
Jonathan Forney <jonathan.forney@gmail.com>
2019-10-31 22:14:54 -0700

    moved date parsing func out of this file

--- src/components/date/DateComp.js
+++ src/components/date/DateComp.js
@@ -3,23 +3,30 @@ import PropTypes from 'prop-types';
 import styles from './Datecomp.css';
 
 function DateComp({ date }) {
-  function todayDateSplit() {
-    var today = new Date();
-    let year = today.getFullYear();
-    let month = today.getMonth() + 1;
-    let day = today.getDate();
-    return `${year}-${month}-${day}`;
-  }
+  //moved this date split func to GetApod where it belongs
+  //leaving old code here for now incase needed for testing
+  //
+  // function todayDateSplit() {
+  //   var today = new Date();
+  //   let year = today.getFullYear();
+  //   let month = today.getMonth() + 1;
+  //   let day = today.getDate();
+  //   return `${year}-${month}-${day}`;
+  // }
 
-  let today = todayDateSplit;
+  // let today = todayDateSplit;
 
   return (
     <div className={styles.datecompdiv}>
       <form>
-        Enter Date: <input type="text"></input>
+        Enter Date: <input type="text">{date}</input>
       </form>
     </div>
   );
 }
+
+DateComp.propTypes = {
+  date: PropTypes.string,
+};
 
 export default DateComp;
