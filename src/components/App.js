0a6ae29a213b2e79e6100323ed87b2cf1241c09e
Jonathan Forney <jonathan.forney@gmail.com>
2019-11-09 22:17:49 -0800

    main header style changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import GetApod from '../containers/apod/GetApod';
 import DateComp from '../components/date/DateComp';
+import styles from '../index.css';
 
 export default function App() {
   return (
     <div>
-      <h1>AstroView</h1>
-      <h2>Helping You Experience the NASA Astronomy Photo Of the Day</h2>
+      <div className={styles.topHead}>
+        <h1>AstroView</h1>
+        <h2>Helping You Experience the NASA Astronomy Photo Of the Day</h2>
+      </div>
       <GetApod />
     </div>
   );
