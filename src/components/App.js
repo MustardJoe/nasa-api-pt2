daa41a62e49d0b9dbb92f6b2d4840031ae59e5ab
Jonathan Forney <jonathan.forney@gmail.com>
2019-11-13 23:24:19 -0800

    got app to center in page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import GetApod from '../containers/apod/GetApod';
-import DateComp from '../components/date/DateComp';
 import styles from '../index.css';
 
 export default function App() {
   return (
-    <div>
+    <div className={styles.app}>
       <div className={styles.topHead}>
         <h1>AstroView</h1>
         <h2>Helping You Experience the NASA Astronomy Photo Of the Day</h2>
