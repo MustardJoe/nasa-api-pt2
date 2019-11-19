3f12ca7bff7db574d035a38ebea3783406dc64b2
Jonathan Forney <jonathan.forney@gmail.com>
2019-11-18 18:04:35 -0800

    updated blurb

--- src/components/introtext/IntroText.js
+++ src/components/introtext/IntroText.js
@@ -3,14 +3,19 @@ import React from 'react';
 export default function IntroText() {
   return (
     <>
-      <p>Welcome to AtroView! I love astronomy and
+      <p>
+        Welcome to AtroView! I love astronomy and
         outer space, and so I created a user-friendly way to browse through the 
         decades of awesome daily astronomy photos provided by the NASA api.
       </p>
       <p>
-        To use this site you must have a key for the NASA api, which you can obtain
-        at <a href="https://api.nasa.gov/">api.nasa.gov/</a>. By default AstroView
-        will load the Astronomy Photo Of the Day (APOD) for today.  Enter a date
+        The NASA api requires a user key to retrive data. AstroView is set up to opperate
+        using the NASA api demo key by default. However, the demo key does have restrictions
+        on the amount of data it will respond to. If you have a NASA api key of your own,
+        you can enter it below and AstroView will use that instead. (Your key is not stored
+        outside of your browser cache). You can obtain your own key at
+        <a href="https://api.nasa.gov/">api.nasa.gov/</a>. By default AstroView
+        will load the Astronomy Picture Of the Day (APOD) for today.  Enter a date
         of your choosing in the date box to view any other APOD. Note: no data
         exists before 1995-6-20.
       </p>
