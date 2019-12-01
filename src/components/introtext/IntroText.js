9fab6a510f753405e1ef22b606f5fa4e05dfdef7
Jonathan Forney <jonathan.forney@gmail.com>
2019-11-30 21:36:38 -0800

    fixed minor problem with link formatting

--- src/components/introtext/IntroText.js
+++ src/components/introtext/IntroText.js
@@ -13,8 +13,8 @@ export default function IntroText() {
         using the NASA api demo key by default. However, the demo key has some data
         restrictions. If you have a NASA api key of your own,
         you can enter it below and AstroView will use that instead. (Your key is never stored
-        anywhere beyond your browser session cache). You can obtain your own key at
-        <a href="https://api.nasa.gov/">api.nasa.gov/</a>. By default AstroView
+        anywhere beyond your browser session cache). You can obtain your own key 
+        at <a href="https://api.nasa.gov/">api.nasa.gov/</a>. By default AstroView
         will load the Astronomy Picture Of the Day (APOD) for today.  Enter a date
         of your choosing in the date box to view any other APOD. Note: no data
         exists before 1995-6-20.
