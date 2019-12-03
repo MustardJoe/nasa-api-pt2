05a2d84737edb6cbd309ef63e73abf91338589d0
Jonathan Forney <jonathan.forney@gmail.com>
2019-12-02 19:11:21 -0800

    added video logic for days when the pic is vid

--- src/components/apod/Apod.js
+++ src/components/apod/Apod.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Apod.css';
+
 const loadingImg = '../../src/assets/loadingimg.gif';
 
 function ApodComp({ astroPhoto, loading }) {
@@ -11,7 +12,28 @@ function ApodComp({ astroPhoto, loading }) {
     hdurl,
     title,
     url,
+    media_type,
   } = astroPhoto;
+  console.log(media_type);
+  
+  if(media_type === 'video') {
+    return (
+      <div className={styles.apodItem}>
+        <h2>{title}</h2>
+        <iframe src={url}
+          frameBorder='0'
+          allow='autoplay; encrypted-media'
+          allowFullScreen
+          title='video'
+        />
+        <p>TKAE ME OUT LATER, testing if video switching logic works {url}</p>
+        <p>Desciption: {explanation}</p>
+        <p>Astronomy Picture of the Day from: {date}</p>
+        <p>SD image: <a href={url}>{url}</a></p>
+        <p>HD Image: <a href={hdurl}>{hdurl}</a></p>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.apodItem}>
@@ -32,6 +54,7 @@ ApodComp.propTypes = {
     hdurl: PropTypes.string,
     title: PropTypes.string,
     url: PropTypes.string,
+    media_type: PropTypes.string,
   }).isRequired,
   loading: PropTypes.bool.isRequired,
 };
