33961734ed822f85d395776b564cd21429432490
Jonathan Forney <jonathan.forney@gmail.com>
2019-12-02 19:10:55 -0800

    changed intro text to be more concise

--- src/components/introtext/IntroText.js
+++ src/components/introtext/IntroText.js
@@ -9,15 +9,9 @@ export default function IntroText() {
         decades of awesome daily astronomy pictures provided by the NASA api.
       </p>
       <p>
-        The NASA api requires a user key to retrive data. AstroView is set up to opperate
-        using the NASA api demo key by default. However, the demo key has some data
-        restrictions. If you have a NASA api key of your own,
-        you can enter it below and AstroView will use that instead. (Your key is never stored
-        anywhere beyond your browser session cache). You can obtain your own key 
-        at <a href="https://api.nasa.gov/">api.nasa.gov/</a>. By default AstroView
-        will load the Astronomy Picture Of the Day (APOD) for today.  Enter a date
-        of your choosing in the date box to view any other APOD. Note: no data
-        exists before 1995-6-20.
+        Upon initial view, AstroView loads with Astronomy Picture Of the Day (APOD) for today. Enter any date
+        in the adjoining box to view other APODs. (Note: no data
+        exists before 1995-6-20.)
       </p>
     </>
   );
